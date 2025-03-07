8bf24318258393eb519d388f897009b6cbb44de7
Wittaya Tangdorn <83308832+DeliciousBoy@users.noreply.github.com>
2025-03-07 12:03:50 +0700

    Add custom styles and extend TailwindCSS configuration for improved theming

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,22 @@ module.exports = {
     './src/**/*.{html,js,jsx,ts,tsx,vue}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'custom-gradient': 'linear-gradient(to top left, #f6fbf4, #f5df99, #5fd068)',
+      },
+      opacity: {
+        '10': '0.1',
+        '20': '0.2',
+        '30': '0.3',
+        '40': '0.4',
+        '50': '0.5',
+        '60': '0.6',
+        '70': '0.7',
+        '80': '0.8',
+        '90': '0.9',
+      },
+    },
   },
   plugins: [
     require('daisyui'),
