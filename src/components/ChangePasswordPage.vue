432367443c78c9a0cf1ac6ef5b2eb6bb3f028d92
Wittaya Tangdorn <83308832+DeliciousBoy@users.noreply.github.com>
2025-03-15 22:30:58 +0700

    Edit(ChangePasswordPage): Refactor form fields and update styling for better user experience

--- src/components/ChangePasswordPage.vue
+++ src/components/ChangePasswordPage.vue
@@ -1,71 +1,51 @@
 <template>
-
-  <div class="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]">
-    <div
-      class="glass-morphism max-w-sm rounded-lg overflow-hidden shadow-lg mx-auto"
-    >
+<div
+    class="absolute h-screen w-screen bg-neutral-950 items-center px-5 py-24 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"
+  >
+    <div class="glass-morphism max-w-sm rounded-lg overflow shadow-lg mx-auto">
       <div class="p-6">
-        <h2 class="text-2xl font-bold text-white mb-2">Change your Password</h2>
-        <p class="text-white mb-6">Please sign in to your account</p>
+        <h4 class="text-2xl text-white  mb-6 flex justify-center">
+          Change Password
+        </h4>
+
         <form @submit.prevent="submitForm">
-          <div class="mb-4">
-            <label class="block text-white font-bold mb-2" for="old_password">
-                Old password
-            </label>
+          <div class="mb-5">
             <input
-              v-model="old_password"
-              class="shadow appearance-none border rounded w-full py-2 px-3 text-white leading-tight focus:outline-none focus:shadow-outline"
-              id="old_password"
+              v-model="username"
+              class="shadow appearance-none border rounded w-full py-2 px-3 text-white leading-tight focus:outline-none focus:shadow-outline border-gray-400/20"
+              id="username"
               type="text"
-              placeholder="Old Password"
+              placeholder="Username"
             />
           </div>
-          <div class="mb-6">
-            <label class="block text-white font-bold mb-2" for="new_password">
-              New Password
-            </label>
+          <div class="mb-5">
             <input
-              v-model="new_password"
-              class="shadow appearance-none border rounded w-full py-2 px-3 text-white leading-tight focus:outline-none focus:shadow-outline"
-              id="new_password"
+              v-model="password"
+              class="shadow appearance-none border rounded w-full py-2 px-3 text-white leading-tight focus:outline-none focus:shadow-outline border-gray-400/20"
+              id="password"
               type="password"
-              placeholder="New Password"
+              placeholder="Password"
             />
           </div>
-          <div class="mb-6">
-            <label class="block text-white font-bold mb-2" for="new_password">
-              Confirm Password
-            </label>
+          <div class="mb-5">
             <input
-              v-model="confirm_password"
-              class="shadow appearance-none border rounded w-full py-2 px-3 text-white leading-tight focus:outline-none focus:shadow-outline"
+              v-model="password"
+              class="shadow appearance-none border rounded w-full py-2 px-3 text-white leading-tight focus:outline-none focus:shadow-outline border-gray-400/20"
               id="confirm_password"
               type="password"
               placeholder="Confirm Password"
             />
           </div>
-          <div class="flex items-center justify-between">
+          <div class="flex items-center justify-center">
             <button
-              class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              class="w-[350px] bg-black h-[40px] my-3 flex items-center justify-center rounded-xl cursor-pointer relative overflow-hidden transition-all duration-500 ease-in-out shadow-md hover:scale-105 hover:shadow-lg before:absolute before:top-0 before:-left-full before:w-full before:h-full before:bg-gradient-to-r before:from-[#009b49] before:to-[rgb(105,184,141)] before:transition-all before:duration-500 before:ease-in-out before:z-[-1] before:rounded-xl hover:before:left-0 text-[#fff]"
               type="submit"
             >
-              Change Password
+              Reset Password
             </button>
-            <a
-              class="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
-              href="#"
-            >
-              Forgot Password?
-            </a>
           </div>
         </form>
-
-        <!--ไปหน้า Register หรือหน้า Change password ปรับตรงนี้ได้เลย-->
-
-        <!-- <div class="mt-4 text-center text-white">
-          <router-link to="/login/register" class="text-blue-500">Register</router-link> |
-          <router-link to="/login/change-password" class="text-blue-500">Change Password</router-link>
-        </div> -->
+        
       </div>
     </div>
   </div>
