ad7c80744263ca5415e48482ec63605a7bc7d761
Wittaya Tangdorn <83308832+DeliciousBoy@users.noreply.github.com>
2025-03-24 16:14:25 +0700

    Add Cart component: Implement basic layout with ShoppingCartIcon

---
+++ src/components/layout/Cart.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="bg-zinc-200 rounded-b-box shadow-md p-4">
+        <ShoppingCartIcon class="h-6 w-6 text-zinc-500" />
+    </div>
+</template>
+
+<script>
+    import {ShoppingCartIcon} from '@heroicons/vue/24/outline'
+</script>
