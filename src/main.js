d738fead93b1492560155bde365310866b9ae18b
Wittaya Tangdorn <83308832+DeliciousBoy@users.noreply.github.com>
2025-03-25 03:58:50 +0700

    Edit(main.js): Integrate vue-toast-notification plugin for enhanced user notifications

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import './assets/main.css'
 import "animate.css";
 import 'font-awesome/css/font-awesome.min.css';
+import ToastPlugin from "vue-toast-notification";
+import "vue-toast-notification/dist/theme-bootstrap.css";
 
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from "./router";
 import './assets/main.css'
 
-createApp(App).use(router).mount("#app");
+createApp(App).use(router, ToastPlugin).mount("#app");
