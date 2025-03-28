4829c7ec15de04efc70154d0a9a4bcdf98343bbb
Wittaya Tangdorn <83308832+DeliciousBoy@users.noreply.github.com>
2025-03-29 01:58:01 +0700

    Refactor ProfileLayout.vue to improve code formatting and remove commented-out component

--- src/layouts/ProfileLayout.vue
+++ src/layouts/ProfileLayout.vue
@@ -1,12 +1,11 @@
 <template>
   <div>
-    <Navbar/>
-  <!-- <ProfileSidebarVue/> -->
+    <Navbar />
+    <!-- <ProfileSidebarVue/> -->
   </div>
 </template>
 
 <script setup>
 // import ProfileSidebarVue from "@/components/layout/ProfileSidebar.vue";
-import Navbar from "@/components/layout/Navbar.vue"
-
+import Navbar from "@/components/layout/Navbar.vue";
 </script>
